49a9d6c798d25de86c813cc05a84a34d2d9045e2
szulf <szymon.mieln@gmail.com>
2025-01-13 15:02:40 +0100

    big changes, switched to json and c++

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### basic understanding
-program interprets breakfile
+program interprets break.json
 based on that runs the necessary compilation commands
 and creates a '.break-cache' file that holds info about last modified change to a file
 
@@ -17,36 +17,7 @@ runs the program
 compiles all files and runs the program
 
 ### breakfile syntax
-#### concepts
-- lists - defined by ` [ item1, item2 ] `  
-    used to define lists of files
-- variables - defined by ` var variable_name = value `  
-    can be used for pretty much anything
-- program variables - defined by ` variable_name = value `  
-    used by the program, and not by the user  
-    list of program variables [here](#program%20variables)
-- comments - defined by ` # `
-
-#### actual syntax
-
-#### program variables
-- project_name
-- source_directory  
-    if left empty defaults to root directory
-- source_files  
-    looks for files in source_directory
-- header_directory  
-    if left empty defaults to root directory
-- header_files  
-    looks for files in header_directory
-- c_compiler  
-    if left empty break will use the compiler specified in the 'CC' environmental variable
-    if that is also not set break will error
-- cpp_compiler  
-    if left empty break will use the compiler specified in the 'CXX' environmental variable
-    if that is also not set break will error
-- c_flags
-- cpp_flags
+json
 
 ### todo
 - add cli commands to change the breakfile  
---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+#include <fstream>
+#include <print>
+#include <string>
+#include <vector>
+
+int main(int argc, char** argv)
+{
+    std::vector<std::string> args{argv, argv + argc};
+
+    if (argc < 2)
+    {
+        std::println("Not enough arguments");
+        std::println("Use 'break help' for help");
+        return 1;
+    }
+
+    if (args[1] == "help")
+    {
+        if (argc == 2)
+        {
+            std::println("BREAK - an attempt at a better make\n");
+            std::println("Commands:\n");
+            std::println("init     start a new break project\n");
+            std::println("build    build all of the files\n");
+        }
+        else
+        {
+            if (args[2] == "init")
+            {
+                std::println("usage: break init project-name\n\n");
+                // TODO
+                // add some more here
+                // like a little description
+            }
+        }
+    }
+    else if (args[1] == "init")
+    {
+        if (argc != 3)
+        {
+            std::println(stderr, "Not enough arguments");
+            std::println(stderr, "Use 'break help init' for help");
+            return 1;
+        }
+
+        // if (std::filesystem::exists("break.json"))
+        // {
+        //     std::println("'break.json' file already exists");
+        //     std::println("please delete it to run this command");
+        // }
+
+        std::ofstream breakfile{"break.json"};
+
+        std::println(breakfile, "{{");
+        std::println(breakfile, "    \"project_name\": \"{}\",", argv[2]);
+        std::println(breakfile, "    \"source_files\": [ \"src/main.cpp\" ],\n");
+        std::println(breakfile, "    \"configs\": [");
+        std::println(breakfile, "        {{ \"debug\": \"-O0 -g -Wall -Werror\" }},");
+        std::println(breakfile, "        {{ \"release\": \"-O3 -DNDEBUG\" }},");
+        std::println(breakfile, "    ],");
+        std::println(breakfile, "}}");
+    }
+    else if (args[1] == "build")
+    {
+        // if (!std::filesystem::exists("build"))
+        // {
+        //     std::filesystem::create_directory("build");
+        // }
+    }
+
+    return 0;
+}
